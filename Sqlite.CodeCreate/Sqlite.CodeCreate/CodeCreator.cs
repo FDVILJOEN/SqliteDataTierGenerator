3798b2aff18287c86c0879ba8614c9084970931c
Francois Viljoen <sourcecodec@hotmail.com>
2023-12-05 11:10:19 +0000

    Initial Commit

---
+++ Sqlite.CodeCreate/Sqlite.CodeCreate/CodeCreator.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Data.Sqlite;
+
+namespace Sqlite.CodeCreate
+{
+    public class CodeCreator
+    {
+        #region Private references
+        SqliteConnection _connection;
+        #endregion
+
+        /// <summary>
+        /// Creates a new instance of the code creator.  
+        /// This class is used to generate c# classes that will interact with the database 
+        /// provided in the connection.
+        /// </summary>
+        /// <param name="dataConnection">Connection to the database</param>
+        public CodeCreator(SqliteConnection dataConnection) 
+        { 
+            this._connection = dataConnection;
+        }
+    }
+}
